fe47f6bc052454db0ca27af9637e5dd626a06996
Rafael Khan <raf.khan.f@gmail.com>
2025-11-27 23:53:51 -0500

    wav list yay

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use std::env;
 use std::fs;
+use std::path::PathBuf;
 
-fn tree(directory: &String) {
+fn tree<'a>(directory: &String, wav_list: &'a mut Vec<PathBuf>) -> &'a mut Vec<PathBuf> {
     let entries = fs::read_dir(directory).unwrap();
     for e in entries {
         match e {
             Ok(entry) => {
-                let path = entry.path();
-                if path.is_dir() {
-                    dbg!(path);
+                let path_buf = entry.path();
+                let path_string = &path_buf.to_str().unwrap().to_string();
+                dbg!(path_string);
+                if path_buf.is_dir() {
+                    tree(path_string, wav_list);
+                } else {
+                    let extension = path_buf.extension();
+                    match extension {
+                        Some(ext) => {
+                            if ext == "wav" {
+                                dbg!(&path_buf);
+                                wav_list.push(path_buf);
+                            }
+                        }
+                        None => {
+                            dbg!("ignored");
+                        }
+                    }
                 }
             }
             Err(_e) => {
@@ -16,10 +32,15 @@ fn tree(directory: &String) {
             }
         }
     }
+
+    return wav_list;
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let dir = &args[1];
-    tree(dir);
+
+    let mut wav_list: Vec<PathBuf> = Vec::new();
+    let updated_wav_list = tree(dir, &mut wav_list);
+    dbg!(updated_wav_list);
 }
